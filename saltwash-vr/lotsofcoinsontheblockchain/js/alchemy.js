d5595856fec2c4ebcdb3d185f83cdedbf6cb93e3
rdmilligan <rdmilligangithub@gmail.com>
2022-09-24 17:09:40 +0100

    Added lots of coins on the blockchain

---
+++ saltwash-vr/lotsofcoinsontheblockchain/js/alchemy.js
@@ -0,0 +1,25 @@
+﻿
+// alchemy mock
+class Alchemy {
+    constructor() {
+        this.core = new class {
+            constructor() {
+                this.state = { blockNumber: 15567265 };
+            }
+
+            getBlockNumber() {
+                this.state.blockNumber++;
+                return this.state.blockNumber;
+            }
+        }
+    }
+}
+
+class AlchemyState {
+    constructor() {
+        this.blockNumber = 0;
+    }
+}
+
+var alchemy = new Alchemy();
+var alchemyState = new AlchemyState();
