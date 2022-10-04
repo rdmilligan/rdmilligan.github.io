dae7e5b950e6661f90fc9af708cbc848bc83d4f7
rdmilligan <rdmilligangithub@gmail.com>
2022-10-04 18:45:33 +0100

    Added plump tomato

---
+++ saltwash-vr/plumptomato/js/pt_alchemy.js
@@ -0,0 +1,13 @@
+﻿
+// alchemy mock
+class Alchemy {
+    constructor() {
+        this.core = new class {
+            getBalance(addressOrName, blockTag = 'latest') {
+                return 0x02c68af0bb140000;
+            }
+        }
+    }
+}
+
+var alchemy = new Alchemy();
