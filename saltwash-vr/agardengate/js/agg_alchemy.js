6c2bf1d8df0af9037e7605af4549d01ef9077f60
rdmilligan <rdmilligangithub@gmail.com>
2022-10-10 17:42:50 +0100

    Added a garden gate

---
+++ saltwash-vr/agardengate/js/agg_alchemy.js
@@ -0,0 +1,13 @@
+﻿
+// alchemy mock
+class Alchemy {
+    constructor() {
+        this.core = new class {
+            getTransaction(transactionHash) {
+                return {from: '0x4B631dA32DDBb757CAdf4b257E72daF6cCB21914'};
+            }
+        }
+    }
+}
+
+const alchemy = new Alchemy();
