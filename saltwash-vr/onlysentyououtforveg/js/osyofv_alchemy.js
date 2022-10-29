26d3d1d53e7e096a0b7746f9d38326c08b6ac987
rdmilligan <rdmilligangithub@gmail.com>
2022-10-29 14:32:58 +0100

    Added only sent you out for veg

---
+++ saltwash-vr/onlysentyououtforveg/js/osyofv_alchemy.js
@@ -0,0 +1,13 @@
+﻿
+// alchemy mock
+class Alchemy {
+    constructor() {
+        this.core = new class {
+            getGasPrice() {
+                return 0x3b2a9a02de;
+            }
+        }
+    }
+}
+
+const alchemy = new Alchemy();
