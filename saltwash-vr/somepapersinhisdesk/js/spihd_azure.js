ea0db167aed37fb7d0613066a2f468c96aedc8bb
rdmilligan <rdmilligangithub@gmail.com>
2022-11-20 19:40:40 +0000

    Added some papers in his desk

---
+++ saltwash-vr/somepapersinhisdesk/js/spihd_azure.js
@@ -0,0 +1,11 @@
+﻿
+// azure mock
+class Azure {
+    getCommand() {
+        return fetch('../json/spihd_azure.json')
+        .then(response => response.json())
+        .then(json => {return json.command;})
+    }
+}
+
+const azure = new Azure();
