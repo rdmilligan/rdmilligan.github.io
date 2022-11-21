248c4e6f171d95ac317a846cf9b98b739d4a8c30
rdmilligan <rdmilligangithub@gmail.com>
2022-11-21 10:09:18 +0000

    Readded some papers in his desk

--- saltwash-vr/somepapersinhisdesk/js/spihd_azure.js
+++ saltwash-vr/somepapersinhisdesk/js/spihd_azure.js
@@ -4,7 +4,7 @@ class Azure {
     getCommand() {
         return fetch('json/spihd_azure.json')
         .then(response => response.json())
-        .then(json => {return json.command;})
+        .then(json => json.command)
     }
 }
 
