16329fb2cdfbd96b4986a08228ff150c194d1fd7
rdmilligan <rdmilligangithub@gmail.com>
2022-12-05 17:49:55 +0000

    Added lips are smacking

---
+++ saltwash-vr/lipsaresmacking/js/las_azure.js
@@ -0,0 +1,11 @@
+﻿
+// azure mock
+class Azure {
+    getCommand() {
+        return fetch('json/las_azure.json')
+        .then(response => response.json())
+        .then(json => json.washTime)
+    }
+}
+
+const azure = new Azure();
