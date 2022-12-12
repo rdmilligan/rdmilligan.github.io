56971f923e604d8e02b990a568186ce434d73595
rdmilligan <rdmilligangithub@gmail.com>
2022-12-12 09:52:24 +0000

    Added rousing the moustaches

---
+++ saltwash-vr/rousingthemoustaches/js/rtm_azure.js
@@ -0,0 +1,11 @@
+﻿
+// azure mock
+class Azure {
+    getWaitTime() {
+        return fetch('json/rtm_azure.json')
+        .then(response => response.json())
+        .then(json => json.waitTime)
+    }
+}
+
+const azure = new Azure();
