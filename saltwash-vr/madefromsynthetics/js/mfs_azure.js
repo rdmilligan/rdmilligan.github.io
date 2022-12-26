bc30f176b973d4ebeeb6a87976a53feeddd497f8
rdmilligan <rdmilligangithub@gmail.com>
2022-12-26 10:05:35 +0000

    Added made from synthetics

---
+++ saltwash-vr/madefromsynthetics/js/mfs_azure.js
@@ -0,0 +1,11 @@
+﻿
+// azure mock
+class Azure {
+    getSecurityCameraPositions() {
+        return fetch('json/mfs_azure.json')
+        .then(response => response.json())
+            .then(json => json.securityCameraPositions)
+    }
+}
+
+const azure = new Azure();
