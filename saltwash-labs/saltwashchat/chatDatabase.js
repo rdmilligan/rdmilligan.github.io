4794031aa3dad988fa71eca5622524f8e751ae1c
rdmilligan <rdmilligangithub@gmail.com>
2023-02-12 15:13:46 +0000

    Added firefly and rat

--- saltwash-labs/saltwashchat/chatDatabase.js
+++ saltwash-labs/saltwashchat/chatDatabase.js
@@ -2,52 +2,52 @@ const chatAnswers = [
   {
     id: 1,
     text: "The world is going to burst with all your questions, man.",
-    keywords: ["how"],
+    keywords: ["how", "tell"],
   },
   {
     id: 2,
     text: "I don't know why, man. I mean, look it up on the world wide web.",
-    keywords: ["why"],
+    keywords: ["why", "does", "help"],
   },
   {
     id: 3,
     text: "If you can step over the spiders, by all means, yes.",
-    keywords: [],
+    keywords: ["door", "outside", "insects"],
   },
   {
     id: 4,
     text: "Maybe try flipping a coin. Heads you fix the puncture.",
-    keywords: [],
+    keywords: ["win", "decide", "bike"],
   },
   {
     id: 5,
     text: "Perhaps the fish needs to be broiled rather than baked. Have you tried that?",
-    keywords: [],
+    keywords: ["cook", "seafood", "recipe"],
   },
   {
     id: 6,
     text: "I've got a pitchfork behind the garage. Mind to put it back when you're done.",
-    keywords: [],
+    keywords: ["gardening", "dig"],
   },
   {
     id: 7,
     text: "It's either right or wrong. Try one then the other.",
-    keywords: [],
+    keywords: ["which", "option", "choose"],
   },
   {
     id: 8,
     text: "Yes. But wait until all the air is out.",
-    keywords: [],
+    keywords: ["tyre", "balloon"],
   },
   {
     id: 9,
     text: "I've not got time for this. My trousers need fixing with a needle and thread.",
-    keywords: [],
+    keywords: ["how", "why"],
   },
   {
     id: 10,
     text: "Well, that's a lifestyle choice, man. But do wear a hard hat.",
-    keywords: [],
+    keywords: ["clothes", "vegan", "sex"],
   },
   {
     id: 11,
@@ -57,31 +57,51 @@ const chatAnswers = [
   {
     id: 12,
     text: "Write your own code, man. I'm not a mule.",
-    keywords: ["code"],
+    keywords: ["code", "programming", "algorithm"],
   },
   {
     id: 13,
     text: "The shops are at the end of the water reservoir. It's a comfortable walk.",
-    keywords: [],
+    keywords: ["shop", "supermarket", "food", "drink", "directions"],
   },
   {
     id: 14,
     text: "I'd suggest removing your shoes. They've been soiled by a mongrel.",
-    keywords: [],
+    keywords: ["house", "door", "visit"],
   },
   {
     id: 15,
     text: "Try turning the compass towards the microwave to repair it.",
-    keywords: [],
+    keywords: ["directions", "search", "fix"],
   },
   {
     id: 16,
     text: "Yes, you can get those nutrients from a bag of beefy crinkle cut.",
-    keywords: ["diet", "eat", "food"],
+    keywords: ["diet", "eat", "food", "crisps"],
   },
   {
     id: 17,
     text: "If you are caught short, well, it's all our garden really.",
-    keywords: [],
+    keywords: ["toilet", "outside", "outdoors", "nature"],
+  },
+  {
+    id: 18,
+    text: "Oh, thanks man. A little sweetness can go a long way.",
+    keywords: ["nice", "kind", "attractive"],
+  },
+  {
+    id: 19,
+    text: "Not sure. It wasn't squeaking when I last used it.",
+    keywords: ["broken", "mechanical", "metal"],
+  },
+  {
+    id: 20,
+    text: "You'd best try the gymnasium, it's open until a quarter past five.",
+    keywords: ["exercise", "sport"],
+  },
+  {
+    id: 21,
+    text: "Crocodiles, I'd say, with the gazelle a close second.",
+    keywords: ["animal", "speed", "fast", "dangerous"],
   },
 ]
